eb4a8fc90fc02243b78ee1ebbd4a39c7b0f1fc12
Kent Rune Henriksen <kentruneh@gmail.com>
2011-03-07 21:36:41 +0100

    Now checking if lotteryUsers.txt exist

--- src/com/bukkit/erbros/Lottery/Lottery.java
+++ src/com/bukkit/erbros/Lottery/Lottery.java
@@ -1,5 +1,7 @@
 package com.bukkit.erbros.Lottery;
 //All the imports
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import org.bukkit.block.Block;
@@ -15,12 +17,31 @@ public class Lottery extends JavaPlugin{
 	@Override
 	public void onDisable() {
 		// TODO Auto-generated method stub
-		
+		System.out.println("Lottery disabled successfully.");
 	}
 
 	@Override
 	public void onEnable() {
 		// TODO Auto-generated method stub
+		
+		// Gets version number and writes out starting line to console.
+		PluginDescriptionFile pdfFile = this.getDescription();
+		System.out.println( pdfFile.getName() + "version " + pdfFile.getVersion() + "is enabled" );
+		
+		// Start Registration. Thanks TheYeti.
+		getDataFolder().mkdirs();
+		
+		// Does the lotteryUsers.txt exist?
+		if (!(new File(getDataFolder(), "lotteryUsers.txt").exists())) {
+            try {
+				new File(getDataFolder(), "lotteryUsers.txt").createNewFile();
+			} catch (IOException e) {
+				
+			}
+        }
+		
+		
+		
 		
 	}
 
