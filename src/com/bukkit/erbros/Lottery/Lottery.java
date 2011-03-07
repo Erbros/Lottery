4d52f0a25916ba5cdd4ab643de6019275763cc0d
Kent Rune Henriksen <kentruneh@gmail.com>
2011-03-07 20:43:33 +0100

    Releasing the first java class.
    Still the noob. But at least I'm still having great courage for the future.

---
+++ src/com/bukkit/erbros/Lottery/Lottery.java
@@ -0,0 +1,27 @@
+package com.bukkit.erbros.Lottery;
+//All the imports
+import java.util.ArrayList;
+import java.util.HashMap;
+import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Event;
+import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.plugin.PluginManager;
+
+
+public class Lottery extends JavaPlugin{
+
+	@Override
+	public void onDisable() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onEnable() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
