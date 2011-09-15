94425a89eb7ea539a43fab3772c4a0649d8012a2
Kent-Rune Henriksen <kentruneh@gmail.com>
2011-09-15 12:18:25 +0200

    Now using custom msg on player join event.

--- src/net/erbros/Lottery/PlayerJoinListener.java
+++ src/net/erbros/Lottery/PlayerJoinListener.java
@@ -3,17 +3,21 @@ package net.erbros.Lottery;
 import org.bukkit.ChatColor;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerListener;
-import net.erbros.Lottery.Lottery;
 
 public class PlayerJoinListener extends PlayerListener {
-	public PlayerJoinListener() {
-	}
+    private Lottery plugin;
+    
+    public PlayerJoinListener(Lottery plugin) {
+        this.plugin = plugin;
+    }
 
-	public void onPlayerJoin(PlayerJoinEvent event) {
-		// Send the player some info about time until lottery draw?
-		event.getPlayer().sendMessage(
-				ChatColor.GOLD + "[LOTTERY] " + ChatColor.WHITE + "Draw in: "
-						+ ChatColor.RED + Lottery.timeUntil(Lottery.nextexec));
-	}
+    @Override
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        // Send the player some info about time until lottery draw?
+        
+        for (String msg : plugin.msgWelcome) {
+            event.getPlayer().sendMessage(msg);
+        }
+    }
 
 }
