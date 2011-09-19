d9bf8a5eba6b2a0f5e337d066976bcd44ad2d7d3
Kent-Rune Henriksen <kentruneh@gmail.com>
2011-09-19 17:55:50 +0200

    Code cleanup

--- src/net/erbros/Lottery/PluginListener.java
+++ src/net/erbros/Lottery/PluginListener.java
@@ -12,36 +12,36 @@ public class PluginListener extends ServerListener {
 	private Methods Methods = null;
 
 	public PluginListener(Lottery plugin) {
-		this.plugin = plugin;
-		this.Methods = new Methods();
+            this.plugin = plugin;
+            this.Methods = new Methods();
 	}
 
 	@Override
 	public void onPluginDisable(PluginDisableEvent event) {
-		if (this.Methods != null && this.Methods.hasMethod()) {
-			Boolean check = this.Methods.checkDisabled(event.getPlugin());
+            if (this.Methods != null && this.Methods.hasMethod()) {
+                Boolean check = this.Methods.checkDisabled(event.getPlugin());
 
-			if (check) {
-				this.plugin.Method = null;
-				System.out
-						.println("[Lottery] Payment method was disabled. No longer accepting payments.");
-			}
-		}
+                if (check) {
+                    this.plugin.Method = null;
+                    System.out
+                            .println("[Lottery] Payment method was disabled. No longer accepting payments.");
+                }
+            }
 	}
 
 	@Override
 	public void onPluginEnable(PluginEnableEvent event) {
-		if (!this.Methods.hasMethod()) {
-			if (this.Methods.setMethod(Bukkit.getPluginManager())) {
-				// You might want to make this a public variable inside your
-				// MAIN class public Method Method = null;
-				// then reference it through this.plugin.Method so that way you
-				// can use it in the rest of your plugin ;)
-				this.plugin.Method = this.Methods.getMethod();
-				System.out.println("[Lottery] Payment method found ("
-						+ this.plugin.Method.getName() + " version: "
-						+ this.plugin.Method.getVersion() + ")");
-			}
-		}
+            if (!this.Methods.hasMethod()) {
+                if (this.Methods.setMethod(Bukkit.getPluginManager())) {
+                    // You might want to make this a public variable inside your
+                    // MAIN class public Method Method = null;
+                    // then reference it through this.plugin.Method so that way you
+                    // can use it in the rest of your plugin ;)
+                    this.plugin.Method = this.Methods.getMethod();
+                    System.out.println("[Lottery] Payment method found ("
+                                    + this.plugin.Method.getName() + " version: "
+                                    + this.plugin.Method.getVersion() + ")");
+                } 
+            }
 	}
 }
