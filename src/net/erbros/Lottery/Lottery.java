ab7a2a037456f376799c601268497f6b3cf520b1
Kent-Rune Henriksen <kentruneh@gmail.com>
2011-10-17 00:50:13 +0200

    working on new configuration

--- src/net/erbros/Lottery/Lottery.java
+++ src/net/erbros/Lottery/Lottery.java
@@ -25,7 +25,6 @@ import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.configuration.InvalidConfigurationException;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Event;
@@ -34,7 +33,6 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.util.config.Configuration;
 
 
 public class Lottery extends JavaPlugin {
@@ -602,7 +600,7 @@ public class Lottery extends JavaPlugin {
 
 		// Is the date we are going to draw the lottery set? If not, we should
 		// do it.
-		if (c.getLong("nextexec") == 0) {
+		if (c.getLong("nextexec",0) == 0) {
 
 			// Set first time to be config hours later? Millisecs, * 1000.
 			nextexec = System.currentTimeMillis() + extendTime();
